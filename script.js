0de0a6a10ad60abf1f011b8ff526b0bbf64b6095
Nikolas Amorim <78495481+nikolasamorim@users.noreply.github.com>
2021-11-22 17:35:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ function calcular(){
 
 //Calculo + Impressão de resposta
     let vf = mensal*((1+juros)**meses-1)/juros.toFixed(2)  
+    
+    
         res.innerHTML = `<b>${nome}</b>, se você aplicar <b>R $${mensal}</b>, 
         à taxa de juros de <b>${tjuros}%</b> ao mês,
         durante <b>${meses}</b> meses,
