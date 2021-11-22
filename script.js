86ab08e67016c64854d3b3bcb09ad001c860ee6d
Nikolas Amorim <78495481+nikolasamorim@users.noreply.github.com>
2021-11-22 17:23:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
+// Recebendo variaveis
 function calcular(){
     const nome = document.getElementById('nome').value
     const mensal = Number(document.getElementById('rendamensal').value)
     const tjuros = Number(document.getElementById('taxajuros').value)
     const meses = Number(document.getElementById('nmrparcelas').value)
-
     const juros = tjuros/100
     
+ // Definindo elemento pelo ID
     var res = document.getElementById("resultado")
-         
-    
 
-   
-    let vf = mensal*((1+juros)**meses-1)/juros.toFixed(2)
-   
-  
+//Calculo + Impressão de resposta
+    let vf = mensal*((1+juros)**meses-1)/juros.toFixed(2)  
         res.innerHTML = `<b>${nome}</b>, se você aplicar <b>R $${mensal}</b>, 
         à taxa de juros de <b>${tjuros}%</b> ao mês,
         durante <b>${meses}</b> meses,
         você acumulrá um total de <b>R $${vf}</b>`   
-    
 }
